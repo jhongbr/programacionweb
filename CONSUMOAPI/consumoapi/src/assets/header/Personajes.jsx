3f5cf2b46a97577c71a51956fa7e6c7101ff5486
jhongbr <jhonstevengbr@gmial.com>
2024-03-24 15:34:02 -0500

    arreglo errores

--- CONSUMOAPI/consumoapi/src/assets/header/Personajes.jsx
+++ CONSUMOAPI/consumoapi/src/assets/header/Personajes.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 
-export const Personajes = ({ personajes = [] }) => {
+export const Personajes = ({personaje = []}) => {
   return (
-    personajes.map((item) => {
-      return (
-        <Card style={{ width: '18rem' }}>
-          <Card.Img src={item.image} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      )
-    })
+    <div className='row'>
+        {
+          personaje.map((item, index) => 
+          {
+            return(
+            <div key={index} className='col'>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img src={item.image} />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            )
+          })
+        }
+    </div>
   )
 }
